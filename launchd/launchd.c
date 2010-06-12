b81b37ab2b11e74c9bbaff0ec73af07fe03c8fef
John Heaton <jheaton.dev@gmail.com>
2010-06-12 16:54:20 -0400

    rdisk0s2 wont mount

--- launchd/launchd.c
+++ launchd/launchd.c
@@ -4,13 +4,42 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <hfs/hfs_mount.h>
+#include <sys/types.h>
 
+#define HANG() while(1){}
+#define HANG_LIMIT(x) { \
+	for(int i=0;i<x;++i){} \
+} \
+
+void setup_console();
+void exec_cmd(char * argv[], char *env[]);
+int setup_disk();
+void welcome_msg();
+void memset_(void *b, int c, int len);
+void fb_new_page();
+void console_write_msg(int fd, const char *msg);
 void console_print(const char *msg);
-void mount_hfs(const char *partition, const char *mnt_point, unsigned int mnt_opts);
-//void sleep(int period);
+int mount_hfs(const char *partition, const char *mnt_point, unsigned int mnt_opts);
 
-int main(int argc, const char *argv[], const char *env[]) {
-	struct stat check;
+int main(int argc, const char *argv[], const char *env[]) {	
+	HANG_LIMIT(20000000);
+	
+	setup_console();
+	fb_new_page();
+	welcome_msg();
+	
+	if(setup_disk() != 0) {
+		console_write_msg(STDERR_FILENO, "*couldn't setup disk\n");
+		HANG();
+	}
+	console_print("*disk set up\n");
+	
+	HANG();
+	
+	return 0;
+}
+
+void setup_console() {
 	int console;
 	
 	close(STDIN_FILENO);
@@ -22,59 +51,158 @@ int main(int argc, const char *argv[], const char *env[]) {
 	dup2(console, STDIN_FILENO);
 	dup2(console, STDOUT_FILENO);
 	dup2(console, STDERR_FILENO);
+}
+
+void welcome_msg() {
+	console_print("*******************\n");
+	console_print("Welcome to Anthrax\n");
+	console_print("*******************\n");
+	console_print("\n\n");
+}
+
+int setup_disk() {
+	struct stat st;
+	int encrypted, fsck_exists;
 	
-	while(stat("/dev/disk0", &check) != 0) {
-		// *** 
+	console_print("*waiting for flash...");
+	while(stat("/dev/disk0", &st) != 0) {
+		HANG_LIMIT(1000);
+	}
+	console_print("done\n");
+	
+	console_print("*checking for /sbin/fsck_hfs...");
+	if(stat("/sbin/fsck_hfs", &st) != 0) {
+		console_print("no\n");
+		fsck_exists = 0;
+	} else {
+		console_print("yes\n");
+		chmod("/sbin/fsck_hfs", 0755);
+		fsck_exists = 1;
 	}
 
-	console_print("Disk mounted\n");
-	
-	mount_hfs("/dev/disk0s1", "/mnt", MNT_ROOTFS | MNT_RDONLY);
-	
-	while(stat("/dev/rdisk0s1", &check) != 0) {
-		// ***
+	console_print("*mounting rootfs(RD, /mnt)...");
+	if(mount_hfs("/dev/disk0s1", "/mnt", MNT_RDONLY) != 0) {
+		console_write_msg(STDERR_FILENO, "failed\n");
+		return -1;
 	}
-	console_print("Filesystem mounted, fscking\n");
+	console_print("done\n");
 	
-	//exec_cmd((const char *[]){ "/mnt/sbin/fsck_hfs", "-fy", "/dev/disk0s1" , (const char *)NULL }, env);
+	console_print("*waiting for rdisk0s1...");
+	while(stat("/dev/rdisk0s1", &st) != 0) {
+		HANG_LIMIT(1000);
+	}
+	console_print("done\n");
 	
-	console_print("\n\n\nWoot\n");
-	while(1) {
+	if(fsck_exists) {
+		exec_cmd((char *[]){"/sbin/fsck_hfs", "-fy", "/dev/rdisk0s1", (char *)0}, NULL);
+	}
+	
+	console_print("*checking for /encrypted...");
+	if(stat("/encrypted", &st) == 0) {
+		encrypted = 1;
+		console_print("yes\n");
 		
+		console_print("*waiting for rdisk0s2s1...");
+		while(stat("/dev/rdisk0s2s1", &st) != 0) {
+			HANG_LIMIT(1000);
+		}
+		console_print("done\n");
+		
+		if(fsck_exists) {
+			exec_cmd((char *[]){"/sbin/fsck_hfs", "-fy", "/dev/rdisk0s2s1", (char *)0}, NULL);
+		}
+	} else {
+		encrypted = 0;
+		console_print("no\n");
+	
+		console_print("*waiting for rdisk0s2...");
+		while(stat("/dev/rdisk0s2", &st) != 0) {
+			HANG_LIMIT(1000);
+		}
+		console_print("done\n");
+		
+		if(fsck_exists) {
+			exec_cmd((char *[]){"/sbin/fsck_hfs", "-fy", "/dev/rdisk0s2", (char *)0}, NULL);
+		}
+	}
+	
+	console_print("*mounting rootfs(RDWR, /mnt)...");
+	if(mount_hfs("/dev/disk0s1", "/mnt", MNT_ROOTFS | MNT_UPDATE) != 0) {
+		console_write_msg(STDERR_FILENO, "failed\n");
+		return -1;
+	}
+	console_print("done\n");
+	
+	console_print("*mounting devfs(/mnt/dev)...");
+	if(mount("devfs", "/mnt/dev", 0, NULL) != 0) {
+		console_write_msg(STDERR_FILENO, "failed\n");
+		return -1;
+	}
+	console_print("done\n");
+	
+	if(encrypted) {
+		console_print("*mounting disk0s2s1(/mnt/var)...");
+		if(mount_hfs("/dev/disk0s2s1", "/mnt/private/var", 0) != 0) {
+			console_write_msg(STDERR_FILENO, "failed\n");
+			return -1;
+		}
+		console_print("done\n");
+	} else {
+		console_print("*mounting disk0s2(/mnt/var)...");
+		if(mount_hfs("/dev/disk0s2", "/mnt/private/var", 0) != 0) {
+			console_write_msg(STDERR_FILENO, "failed\n");
+			return -1;
+		}
+		console_print("done\n");
 	}
 	
 	return 0;
 }
 
-void console_print(const char *msg) {
-	// *** Investigate: why are we writing 1 byte at a time?
+void console_write_msg(int fd, const char *msg) {
+	if(fd < 0 || msg == NULL) return;
+	
 	while(*msg != '\0') {
-		write(STDOUT_FILENO, msg, 1);
+		write(fd, msg, 1);
 		msg++;
 	}
 }
 
-void mount_hfs(const char *partition, const char *mnt_point, unsigned int mnt_opts) {
+void console_print(const char *msg) {
+	console_write_msg(STDOUT_FILENO, msg);
+}
+
+void fb_new_page() {
+	for(int i=0;i<100;++i) {
+		console_print("\n");
+	}
+}
+
+int mount_hfs(const char *partition, const char *mnt_point, unsigned int mnt_opts) {
 	struct hfs_mount_args args;
+	memset_(&args, '\0', sizeof(args));
 	args.fspec = (char *)partition;
 	
 	if(mount("hfs", mnt_point, mnt_opts, &args) != 0) {
-		console_print("Error mounting disk...\n");
-		reboot(0); // reboot, we failed.
+		return -1;
+	}
+	
+	return 0;
+}
+
+void memset_(void *b, int c, int len) {
+	int index;
+	for(index = 0; index < len; ++index) {
+		((char *)b)[index] = c;
 	}
 }
 
 void exec_cmd(char * argv[], char *env[]) {
 	if(vfork() != 0) {
 		while(wait4(-1, NULL, WNOHANG, NULL) <= 0) {
-			
+			HANG_LIMIT(1000);
 		}
 	} else {
 		execve(argv[0], argv, env);
 	}
 }
-
-//
-//void sleep(int period) {
-//	
-//}
