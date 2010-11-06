d82216bb9e862c3e1334e44b4d0d7073ad3a93a3
Joshua Hill <posixninja@gmail.com>
2010-11-06 12:37:53 -0400

    cleaned out directory

--- rdkit/framebuffer.h
+++
@@ -1,5 +0,0 @@
-#include <CoreGraphics/CoreGraphics.h>
-
-CGContextRef framebuffer_open();
-CGLayerRef create_image_layer(CGContextRef screen, const char *path);
-void framebuffer_close(CGContextRef fb_context);
